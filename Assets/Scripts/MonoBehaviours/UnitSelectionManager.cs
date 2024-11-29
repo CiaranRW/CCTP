980789196bf859355bdba15e8c2a0ab95bac5c33
Ciaran Williams <ciaran2.williams@uwe.live.ac.uk>
2024-11-29 18:07:53 +0000

    Implementation of DOTS system

---
+++ Assets/Scripts/MonoBehaviours/UnitSelectionManager.cs
@@ -0,0 +1,32 @@
+using Unity.Entities;
+using UnityEngine;
+using Unity.Collections;
+
+public class UnitSelectionManager : MonoBehaviour
+{
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            Vector3 mouseWorldPosition = MouseWorldPosition.Instance.GetPosition();
+
+            EntityManager entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
+            EntityQuery entityQuery = new EntityQueryBuilder(Allocator.Temp).WithAll<UnitMover>().Build(entityManager);
+
+            NativeArray<Entity> entityArray = entityQuery.ToEntityArray(Allocator.Temp);
+            NativeArray<UnitMover> unitMoverArray = entityQuery.ToComponentDataArray<UnitMover>(Allocator.Temp);
+
+            for (int i = 0; i < unitMoverArray.Length; i++)
+            {
+                UnitMover unitMover = unitMoverArray[i];
+                unitMover.targetPosition = mouseWorldPosition;
+                //unitMoverArray[i] = unitMover;
+                entityManager.SetComponentData(entityArray[i], unitMover);
+            }
+            //entityQuery.CopyFromComponentDataArray(unitMoverArray);
+
+        }
+    }
+
+}
